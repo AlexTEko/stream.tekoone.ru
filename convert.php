3f0b27f1c7e468b90c8a6fb4663a10b9ecebda59
Alexander Teseyko <alexteko@gmail.com>
2015-09-08 18:24:13 +0300

    full width

--- convert.php
+++ convert.php
@@ -8,17 +8,17 @@
                 $file_name = explode(".", $file);
                 if (end($file_name) == "flv")
 				{
-                    exec("avconv -i ".$directory.$file." -codec copy ".$directory.$file.".mp4 ");
+                    exec("avconv -i ".$directory.$file." -codec copy -profile baseline ".$directory.$file.".mp4 ");
 					exec("avconv -i ".$directory.$file." -r 1 -s 1280x720 -f image2 ".$directory.$file.".jpeg");
 					exec("avconv -i ".$directory.$file." -r 1 -s 40x32 -f image2 ".$directory.$file.".min.jpeg");
-					
+
 					rename($directory.$file.".mp4", $rec.$file.".mp4");
 					rename($directory.$file.".jpeg", $rec.$file.".jpeg");
 					rename($directory.$file.".min.jpeg", $rec.$file.".min.jpeg");
-					
+
 					unlink($directory.$file);
 				}
             }
         }
-		
+
 		echo "Ok!";
